1f2ba74366686342d89f6354388f786051717c77
briccm84 <msbricco@gmail.com>
2022-08-09 10:25:38 -0500

    removed unused imports

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -2,10 +2,6 @@ package chess;
 
 import Display.ChessDisplay;
 
-import java.util.ArrayList;
-
-import static javafx.application.Application.launch;
-
 public class Main {
     public static void main(String args[]){
         ChessDisplay chessDisplay = new ChessDisplay();
